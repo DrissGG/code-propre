ac6a41a9339bd44b4524ec82d5a11da1c2e106ba
Driss <drissgazzaoui1986@hotmail.com>
2024-01-31 11:30:00 +0100

    ex2 corrige

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,25 @@
 package ex2;
 
 public class LivretA extends CompteBancaire {
+	private double tauxRemuneration;
+	
+	/** solde : solde du compte */
+	private double solde;
 
 	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+		super(type, solde, 0);
+	}
+	
+	public void appliquerRemuAnnuelle(){
+		setSolde(getSolde() -tauxRemuneration/100);
+		}
+	
+	
+	@Override
+	public void debiterMontant(double montant) {
+	    if (getSolde()- montant > 0) {
+	        this.solde -= montant;
+	    }
 	}
 
 }
